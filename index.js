57b4b62fae234ea991bb485e5d732c13176364c9
sagar2005 <jaidunna1117@gmail.com>
2020-09-13 14:42:03 -0500

    first cmmit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let name = 'Mosh'; // String Literal
+let age = 30; // Number Literal
+let isApproved = false;  // boolean Literal
+let fristName = undefined;
+let selectedColor = null;
+function calc(){    
+alert("test 123");
+const a=document.getElementById("input1");
+const b=document.getElementById("input2");
+alert(a.value+b.value);
+}
